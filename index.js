9847db779f5dd233b722866cf21ddd084004ab9f
neo1710 <neeraj3dworld@gmail.com>
2023-07-17 10:22:26 +0530

    work

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const userRoute = require('./routes/userRoutes');
-const cors = require('cors');
+const cors=require('cors')
 require("dotenv").config();
+
 const app=express();
 app.use(express.json());
+app.use(cors());
+
 app.use('/users',userRoute);
-app.use(cors())
+
 
 
 app.get('/',(req,res)=>{
