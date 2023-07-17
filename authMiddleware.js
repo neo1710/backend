7f2f88d6653397b15c040683cf931d97a9ac3f7c
neo1710 <neeraj3dworld@gmail.com>
2023-07-17 09:43:25 +0530

    backend file

---
+++ authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt=require('jsonwebtoken');
+const bkModel = require('./models/bkModel');
+
+const auth=async(req,res,next)=>{
+   try {
+    const {authorization}=req.headers;
+    const token=authorization.split(" ")[1];
+    const user=jwt.verify(token,'neo');
+    if(!user){
+        res.status(400).send({msg:'unauthorized token'});
+    }
+    console.log(user);
+    const check=await bkModel.findOne({token});
+    if(check){
+        res.status(400).send({msg:'login first'});
+    }else{
+     next();
+    }
+   } catch (error) {
+    res.status(400).send({msg:error});
+   }
+ 
+}
+
+module.exports=auth;
